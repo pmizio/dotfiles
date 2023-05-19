8c3c0fecbd874916c76bb1fa48b7c91c07c35929
Paweł Mizio <mizio.pawel@gmail.com>
2023-05-19 11:32:41 +0200

    feat: tmux mouse support and install rust

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 set -g fish_greeting
 
+fish_add_path "$HOME/.cargo/bin/"
+
 eval "$(/usr/local/bin/brew shellenv)"
-fnm env --use-on-cd | source
+fnm env | source
 starship init fish | source
 zoxide init fish | source
