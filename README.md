0ada9b152b4e6ba9058583cb059c1be5e70b2d52
Paweł Mizio <mizio.pawel@gmail.com>
2023-05-27 17:37:22 +0200

    feat: add wsl support (#1)

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ brew install ansible
 
 # run bootstrap playbook
 ansible-playbook bootstrap.yml
+
+# run bootstrap playbook(Linux/WSL)
+ansible-playbook bootstrap.yml -K
 ```
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,13 @@ set -g fish_greeting
 
 fish_add_path "$HOME/.cargo/bin/"
 
-eval "$(/usr/local/bin/brew shellenv)"
+set -l os (uname)
+
+if test "$os" = Darwin
+  eval "$(/usr/local/bin/brew shellenv)"
+  set -gx SHELL /usr/local/bin/fish
+end
+
 fnm env | source
 starship init fish | source
 zoxide init fish | source
