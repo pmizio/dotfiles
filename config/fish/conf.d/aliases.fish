252467a7ca643701b55a38474ea32ac9dd837b33
Paweł Mizio <mizio.pawel@gmail.com>
2023-06-01 22:21:20 +0200

    feat: probably fix `fish` config for both OSes

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -6,7 +6,6 @@ function vim --wraps nvim
   nvim $argv;
 end
 
-alias update_nvim="brew uninstall neovim && brew install neovim --HEAD"
 alias setnode="fnm use (jq -r .engines.node package.json | sed -E 's/[0-9.]+ - //')"
 
 alias ls="exa"
@@ -14,3 +13,16 @@ alias ll="exa -lbF --git"
 alias la="exa -lbhHigUmuSa --time-style=long-iso --git --color-scale"
 
 alias cat="bat"
+
+function update_nvim
+  if test (uname) = Darwin
+    brew uninstall neovim
+    brew install neovim --HEAD
+  else
+    set from_dir (pwd)
+    cd $HOME/.neovim
+    make distclean
+    make CMAKE_BUILD_TYPE=Release
+    cd $from_dir
+  end
+end
