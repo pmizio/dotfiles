70a4e38e4265e1f48c4dee392e4af1c72a334125
Paweł Mizio <pawel.mizio@allegro.com>
2023-11-19 23:09:17 +0100

    fix: cleanup bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,4 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon
 mkdir -p $HOME/.config/nix
 echo "experimental-features = nix-command flakes" > $HOME/.config/nix/nix.conf
 source $HOME/.profile
-#nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-#nix-channel --update
-#nix-shell '<home-manager>' -A install
 nix run home-manager/master -- switch -b backup --flake .#wsl
-#home-manager switch -b backup --flake .#wsl
-#nix run . --switch --flake .#wsl
